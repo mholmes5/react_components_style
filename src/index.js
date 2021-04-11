f4c52206541dc425cb505086a6bd05f14068c259
Michael Holmes <michaelholmes618@gmail.com>
2021-04-11 12:16:40 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Grilled Shrimp</li>
+      <li>Ribs</li>
+      <li>Spaghetti</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
