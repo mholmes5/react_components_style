73ca471811e5f46878740a96c73a3f50d3b34026
Michael Holmes <michaelholmes618@gmail.com>
2021-04-11 12:41:31 -0500

    Complete component refactor

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Grilled Shrimp</li>
+      <li>Ribs</li>
+      <li>Spaghetti</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Grilled Shrimp</li>
-      <li>Ribs</li>
-      <li>Spaghetti</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
